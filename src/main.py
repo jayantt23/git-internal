496c0483463243782c220de2a8c3f7d3d4c5b95f
jayantt23 <jayantsharma1864@gmail.com>
2026-04-07 14:35:53 +0000

    implement init and hash-object command

--- src/main.py
+++ src/main.py
@@ -1,10 +1,44 @@
 import argparse
 import sys
 import os
+import hashlib
+import zlib
 
-def cmd_init(args):
-    # init Logic
-    print("Initializing a new git-internal repository...")
+def cmd_init(repo_path="."):
+    """Creates the basic Git directory structure."""
+    git_dir = os.path.join(repo_path, ".git")
+    if(os.path.exists(git_dir)):
+        print(f"Directory already exists: {git_dir}")
+        return
+    
+    # Create directories
+    os.makedirs(os.path.join(git_dir, "objects"), exist_ok=True)
+    os.makedirs(os.path.join(git_dir, "refs", "heads"), exist_ok=True)
+    
+    # Create the HEAD file
+    with open(os.path.join(git_dir, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+     
+    print(f"Initialized empty Git repository in {os.path.abspath(git_dir)}")
+
+def hash_object(data, obj_type="blob", write=True):
+    """Hashes an object, optionally writing it to the object store."""
+    # Header: "type size\x00"
+    header = f"{obj_type} {len(data)}".encode() 
+    full_data = header + b"\x00" + data
+    
+    sha1 = hashlib.sha1(full_data).hexdigest()
+    
+    if write:
+        # Path : .git/objects/first2/remaining38
+        path = os.path.join(".git", "objects", sha1[:2], sha1[2:])
+        if not os.path.exists(path):
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            with open(path, "wb") as f:
+                f.write(zlib.compress(full_data))
+    
+    return sha1
+    
 
 def main():
     parser = argparse.ArgumentParser(description="A mini-git implementation from scratch.")
@@ -13,11 +47,19 @@ def main():
     # init command
     init_parser = subparsers.add_parser("init", help="Initialize a new repo")
     init_parser.add_argument("path", default=".", nargs="?", help="Where to create the repository")
+    
+    # hash-object
+    hash_parser = subparsers.add_parser("hash-object")
+    hash_parser.add_argument("file", help="The file to hash")
+    hash_parser.add_argument("-w", action="store_true", help="Write the object to the database")
 
     args = parser.parse_args()
 
     if args.command == "init":
-        cmd_init(args)
+        cmd_init(args.path)
+    elif args.command == "hash-object":
+        with open(args.file, "rb") as f:
+            print(hash_object(f.read(), write=args.w))
     elif args.command is None:
         parser.print_help()
 
